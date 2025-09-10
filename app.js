9f0962b4f8b3015dab4607871390640358db0439
MaheshVenneti <maheshvenneti123@mail.com>
2025-09-10 11:08:23 +0530

    variable declarations and scope examples

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('Data check')
+//console.log('Data check')
+
+//variables
+
+// var,let and const
+
+// functional sope 
+// var
+// function test(){
+
+// }
+
+// block scope
+// let and const
+// {
+
+// }
+
+// var name = 'mahes'
+// var name = 'suresh' // allowed
+
+// let count = 10
+//  count = 20 
+
+// const age = 18
+// age = 22
+// console.log(age)
