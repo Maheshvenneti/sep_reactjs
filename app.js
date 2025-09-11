ffbbe8298ba3822b034ff73a8f4a371378e58695
MaheshVenneti <maheshvenneti123@mail.com>
2025-09-11 11:05:40 +0530

    Update comments for data types and examples in app.js

--- app.js
+++ app.js
@@ -25,3 +25,34 @@
 // const age = 18
 // age = 22
 // console.log(age)
+
+//Data Types:-
+
+// -> Primitive Data type
+// -> Non Primitive Data type
+
+// 1. string -  "string"
+// 2. Number- 1234
+// 3. Boolean- true/false
+
+// 4. Undefined 
+// let a; // declared but not assigned
+// console.log(a)
+// 5. Null
+// let a = null; // intentional empty value
+
+
+// -> Non Primitive Data type
+//object
+// let address = {
+//     "city":"Hyderabad",
+//     "state":"TS",
+// }
+//Array
+// let fruits = [{},{},{}]
+
+//function
+// function fire(){
+//     console.log("function fire")
+// }
+// fire();
